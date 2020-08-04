89f9ca7b8b4352b4e5b8f3bb2f866a38e509a08d
Jack Lo <jack.lo@tbcasoft.com>
2020-08-04 13:40:12 +0800

    Make requestHandlers

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    const { pathname } = url.parse(request.url);
+    console.log(`Request for ${pathname} received.`); // This will be called twice. 因為大部分伺服器都會在你存取 http://localhost:8888 /時嘗試讀取 http://localhost:8888/favicon.ico
+
+    let postData = '';
+    request.setEncoding('UTF-8');
+    request.addListener('data', (postDataChunk) => {
+      postData += postDataChunk;
+      console.log(`Received POST data chunk '${postDataChunk}'.`);
+    });
+    request.addListener('end', () => {
+      route(handle, pathname, response, postData);
+    });
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
+
+exports.start = start;
